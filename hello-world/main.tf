1dcbb394fd7ffe313de41b5ce980d42f6a422a06
Amry Amanah <amry.amanah@gmail.com>
2018-09-13 06:34:30 +0700

    one simple webserver

--- hello-world/main.tf
+++ hello-world/main.tf
@@ -1,6 +1,5 @@
 terraform {
-#   required_version = ">= 0.8, < 0.9"
-    required_version = ">= 0.8"
+  required_version = ">= 0.8"
 }
 
 provider "aws" {
@@ -8,10 +7,32 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-51537029"
-  instance_type = "t2.micro"
+  ami                    = "ami-51537029"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
   tags {
-      Name = "terraform-example"
+    Name = "terraform-example"
   }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
 }
